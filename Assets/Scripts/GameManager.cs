ce6edeba00e2878a8815c1b6b31f2c7aebd61a53
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-03-30 15:58:07 +0200

    Dodano Interfejs
    Wyświetlanie punktów i powiadomienie po strzeleniu bramki

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private int score_Blue = 0;
     private int score_Red = 0;
+    public Text ScoreText;
+    public Text GoalText;
+    public GameObject GoalTextPanel;  
 
     void Start()
     {
@@ -18,20 +22,28 @@ public class GameManager : MonoBehaviour
         if (team == "blue")
         {
             score_Blue++;
+            GoalText.color = Color.blue;
         }
         else if (team == "red")
         {
             score_Red++;
+            GoalText.color = Color.red;
         }
-        Debug.Log(score_Blue + " : " + score_Red);
-        //scoreText.text = "Score: " + score.ToString();
-        Debug.Log(team + " scores");
-        Invoke("RestartScene", 1.0f);
+        ScoreText.text = score_Blue + " : " + score_Red;
+        GoalText.text = team.ToUpper() + " TEAM SCORES";
+        GoalTextPanel.SetActive(true);
+        //Invoke("RestartScene", 1.0f);
+        Invoke("Foo", 1.0f);
         
     }
 
     private void RestartScene()
     {
         SceneManager.LoadScene("StartScene");
+    }
+
+    private void Foo()
+    {
+        GoalTextPanel.SetActive(false);
     }
 }
