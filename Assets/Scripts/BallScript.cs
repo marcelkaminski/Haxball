5485d68d0ed276b5465b15f98fa0017f8b44db81
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-03-30 12:40:54 +0200

    Dodano Kopanie
    Gracz po wciśnięciu spacji zmienia atrybut kicking na True, piłka sprawdza czy obiekt który z nią koliduje jest graczem i czy "kopie", jeśli tak, jest dodawana siła przeciwna do kierunku styku z graczem

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float kickForce = 1000;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        Debug.Log(other.transform.GetComponent<PlayerScript>().kicking);
+        if(other.transform.CompareTag("Player") && other.transform.GetComponent<PlayerScript>().kicking)
+        {
+            Vector2 direction = (other.transform.position - transform.position).normalized;
+            rb.AddForce(-direction * kickForce);
+            Debug.Log("kicked" + (-direction*kickForce));
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     private float moveHorizontal;
     private float moveVertical;
     public float playerSpeed;
+    public bool kicking = false;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -14,6 +15,11 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        PlayerKick();
     }
 
     void FixedUpdate()
@@ -28,5 +34,19 @@ public class PlayerScript : MonoBehaviour
 
         movement = new Vector2(moveHorizontal, moveVertical);
         rb.velocity = movement * playerSpeed;
+    }
+
+    private void PlayerKick()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            kicking = true;
+            Invoke("StopKicking", 0.1f);
+        }
+    }
+
+    private void StopKicking()
+    {
+        kicking = false;
     }
 }
