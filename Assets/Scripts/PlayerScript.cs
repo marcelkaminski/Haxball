b3d0fd9bb9d4e054aaf34ba5287bd2e066086318
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-03-30 11:10:40 +0200

    Dodano postać gracza porusza się
    Do postaci gracza dodano skrypt - PlayerScript, w którym dodano funkcje PlayerMovement(). W skrypcie dodano publiczny parametr playerSpeed, który można zmieniać. Zmieniono Update na FixedUpdate. https://www.youtube.com/watch?v=km-04aUJy4o#

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float moveHorizontal;
+    public float moveVertical;
+    public float playerSpeed;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * playerSpeed;
+    }
+}
